ddf718bc6351bf31c1731266201e050c514f2d16
ROWEND36 <rowendduke36@gmail.com>
2023-11-07 08:28:40 +0100

    feat: latest

---
+++ components/teacher/AcademicsPage.js
@@ -0,0 +1,45 @@
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import PageHeader from "../PageHeader";
+import Card2 from "../Card2";
+
+import { Add, ArchiveAdd } from "iconsax-react";
+import { useQuery } from "@/models/lib/query";
+import Students from "@/models/student";
+import Teachers from "@/models/teacher";
+import Parents from "@/models/parent";
+import SessionSelect from "../SessionSelect";
+import { Button, Tab, Tabs } from "@mui/material";
+import ModelFormDialog from "../ModelFormDialog";
+import { useEffect, useState } from "react";
+import AssignedCourses from "@/models/course";
+import { supplyModelValues } from "../ModelDataView";
+import ThemedTable from "../ThemedTable";
+import ClassesTable from "./ClassesTable";
+import CoursesTable from "./CoursesTable";
+import SessionsTable from "./SessionsTable";
+
+const TABS = [
+  {
+    model: AssignedCourses,
+    header: "Courses",
+    name: "course",
+  },
+];
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
+
+export default function AcademicsPage() {
+  return (
+    <Box sx={{ backgroundColor: "background.default", minHeight: "100vh" }}>
+      <SessionsTable />
+      <CoursesTable />
+      <ClassesTable />
+    </Box>
+  );
+}
---
+++ components/teacher/ClassesTable.js
@@ -0,0 +1,6 @@
+import ClassRooms from "@/models/classroom";
+import ModelTable from "../ModelTable";
+
+export default function ClassesTable() {
+  return <ModelTable Model={ClassRooms} />;
+}
