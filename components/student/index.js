f1f005588e980624fdb91805e9694c4416a31b1a
ROWEND36 <rowendduke36@gmail.com>
2023-11-13 14:24:03 +0100

    feat: remove student links

--- components/student/index.js
+++ components/student/index.js
@@ -7,45 +7,14 @@ import {
   UserEdit,
   WalletMoney,
 } from "iconsax-react";
-import Overview from "./OverviewPage";
 import Transactions from "./TransactionsPage";
-import Users from "./UsersPage";
-import Events from "./EventsPage";
-import Academics from "./AcademicsPage";
-import UserRedirect from "../UserRedirect";
-import RegistrationsPage from "./RegistrationsPage";
 import Head from "next/head";
 import SettingsPage from "./SettingsPage";
 const TABS = [
   {
-    name: "Overview",
-    icon: Element3,
-    component: Overview,
-  },
-  {
     name: "Transactions",
     icon: WalletMoney,
     component: Transactions,
-  },
-  {
-    name: "Users",
-    icon: UserEdit,
-    component: Users,
-  },
-  {
-    name: "Academics",
-    icon: Teacher,
-    component: Academics,
-  },
-  {
-    name: "Registrations",
-    icon: UserAdd,
-    component: RegistrationsPage,
-  },
-  {
-    name: "News & Events",
-    icon: Calendar,
-    component: Events,
   },
   {
     name: "Settings",
